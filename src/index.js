9d2a7570b53b14c84526e9e171e302a720643e2b
Aleksandr Punov <Dempfil@tut.by>
2022-01-12 16:32:24 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = n + '';
+        let reversedText = n.split('').reverse().join('');
+        let reverse = parseInt(reversedText, 10);
+    console.log("reversed number: ", reverse);
+        return reverse;
 }
